6401f942b08ad6f3641833f88742a816410975f2
rafael <rafassa95@gmail.com>
2025-04-02 16:58:25 -0300

    save 4

--- js/form.js
+++ js/form.js
@@ -3,11 +3,12 @@
 
 class contato {
 
-    constructor(nome,  email, telefone, contato){
+    constructor(nome,  email, telefone, contato, texto){
         this.nome = nome
         this.email = email
         this.telefone = telefone 
         this.contato = contato
+        this.texto = texto
     }
     
 }
@@ -15,12 +16,10 @@ class contato {
 function Post(form) {
 
   let data = new contato(form.elements.namedItem("nome").value,
-            
             form.elements.namedItem("email").value, 
-         
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-
+            form.elements.namedItem("contato").value,
+            form.elements.namedItem("texto").value);
 
             console.log(data)
 
@@ -35,5 +34,5 @@ function Enviar() {
     if (nome.value != "") {
         alert('Obrigado pessoa usuaria ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-
-}
+    
+};
